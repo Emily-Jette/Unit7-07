23ed4e2824accd737d141bcc3112b3d3fb432a57
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-26 13:18:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Setting up variables 
+let firstNum = 0
+let secondNum = 0
+let counter = 0
+let answer = 0 
+
+document.getElementById('enter').addEventListener('click', calculator)
+
+function calculator () {
+  // restting so multiplication is correct
+  answer = 0 
+  // getting values from text box and convert to integer
+  firstNum = document.getElementById('num1').value
+  firstNum = parseInt(firstNum)
+  secondNum = document.getElementById('num2').value
+  secondNum = parseInt(secondNum)
+  // uses for loop to multiply
+  for (counter = 0; counter < firstNum; counter++) {
+    answer = answer + secondNum
+  }
+  document.getElementById('product').innerHTML = answer
+}
