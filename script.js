abd2a8b79b8a4095fe3fd096e483879226f4580d
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-26 13:29:48 +0000

    fixed trailing spaces

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Setting up variables 
+// Setting up variables
 let firstNum = 0
 let secondNum = 0
 let counter = 0
-let answer = 0 
+let answer = 0
 
 document.getElementById('enter').addEventListener('click', calculator)
 
 function calculator () {
   // restting so multiplication is correct
-  answer = 0 
+  answer = 0
   // getting values from text box and convert to integer
   firstNum = document.getElementById('num1').value
   firstNum = parseInt(firstNum)
